60ff5490cccf5d5fa2554c75383681fbc1c5c49e
kedorev <kedorev@gmail.com>
2017-08-22 16:05:55 +0200

    Adding Utils

--- app/src/AppBundle/Utils/LocalisationUtils.php
+++ app/src/AppBundle/Utils/LocalisationUtils.php
@@ -34,7 +34,7 @@ namespace AppBundle\Utils;
         
         
                 //calcul précis
-                $dp= 2 * asin(sqrt(pow (sin(($lat1-$lat2)/2) , 2), cos($lat1)*cos($lat2)* pow( sin(($lon1-$lon2)/2) , 2)));
+         $dp= 2 * asin(sqrt(pow (sin(($lat1-$lat2)/2) , 2) + cos($lat1)*cos($lat2)* pow( sin(($lon1-$lon2)/2) , 2)));
         
                 //sortie en km
                 $d = $dp * $r;
@@ -44,4 +44,54 @@ namespace AppBundle\Utils;
         
                 return $d;
      }
+
+
+     public static function getCoordonateByAddress($address)
+     {
+         // url encode the address
+         $address = urlencode($address);
+
+         // google map geocode api url
+         $url = "http://maps.google.com/maps/api/geocode/json?address={$address}";
+
+         // get the json response
+         $resp_json = file_get_contents($url);
+
+         // decode the json
+         $resp = json_decode($resp_json, true);
+
+         // response status will be 'OK', if able to geocode given address
+         if($resp['status']=='OK'){
+
+             // get the important data
+             $lati = $resp['results'][0]['geometry']['location']['lat'];
+             $longi = $resp['results'][0]['geometry']['location']['lng'];
+             $formatted_address = $resp['results'][0]['formatted_address'];
+
+             // verify if data is complete
+             if($lati && $longi && $formatted_address){
+
+                 $loc = new Localisation($lati,$longi);
+
+                 // put the data in the array
+                 $data_arr = array();
+
+                 array_push(
+                     $data_arr,
+                     $loc,
+                     $formatted_address
+                 );
+
+                 return $data_arr;
+
+             }else{
+                 return false;
+             }
+
+         }else{
+             return false;
+         }
+     }
+
+
  } 
